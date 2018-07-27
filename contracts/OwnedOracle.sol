dca1cb16cb0d01eb43be143fcaaae9323c64103f
Chris Whinfrey <cwhinfrey@gmail.com>
2018-07-27 01:10:40 -0700

    Add OwnedOracle functionality

--- contracts/OwnedOracle.sol
+++ contracts/OwnedOracle.sol
@@ -1,7 +1,12 @@
 pragma solidity ^0.4.24;
 
 import './BasicOracle.sol';
+import 'zeppelin-solidity/contracts/ownership/Ownable.sol';
 
-contract OwnedOracle is BasicOracle {
+contract OwnedOracle is BasicOracle, Ownable {
+
+  function setResult(bytes32 _result) public onlyOwner {
+    _setResult(_result);
+  }
 
 }
