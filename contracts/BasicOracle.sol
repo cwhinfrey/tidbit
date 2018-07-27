c34866a59ff306f6e376c7178abc558f76d0e685
Chris Whinfrey <cwhinfrey@gmail.com>
2018-07-27 01:09:32 -0700

    Add BasicOracle functionality

--- contracts/BasicOracle.sol
+++ contracts/BasicOracle.sol
@@ -1,7 +1,25 @@
 pragma solidity ^0.4.24;
 
-import './BasicOracle.sol';
+import './IOracle.sol';
 
-contract BasicOracle is IOracle {
+contract BasicOracle /*is IOracle */{
+
+  bytes32 public result;
+  bool public isResultSet;
+
+  /* 
+   *  Internal functions
+   */
+
+  function _setResult(bytes32 _result) internal {
+    require(!isResultSet);
+    result = _result;
+    isResultSet = true;
+    _resultWasSet(_result);
+  }
+
+  function _resultWasSet(bytes32 _result) internal {
+    // optional override
+  }
 
 }
