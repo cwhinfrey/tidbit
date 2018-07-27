ceea4c62ef6e210b21167be90c13e742fee00ad6
Chris Whinfrey <cwhinfrey@gmail.com>
2018-07-27 01:13:59 -0700

    squash

--- test/OwnedOracle.test.js
+++ test/OwnedOracle.test.js
@@ -1,7 +1,15 @@
+const { toAscii } = require('web3-utils')
 const OwnedOracle = artifacts.require('OwnedOracle')
 
 require('chai').should()
 
 contract('OwnedOracle', (accounts) => {
-  
+  it('can set result by owner', async () => {
+    const RESULT = 'hello oracle'
+    
+    const oracle = await OwnedOracle.new()
+    await oracle.setResult(RESULT)
+    const result = await oracle.result()
+    toAscii(result).replace(/\u0000/g, '').should.equal(RESULT)
+  })
 })
