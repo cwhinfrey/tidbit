49e4b4248b3f21563ca65d1efcf9fafee6a438fa
Chris Whinfrey <cwhinfrey@gmail.com>
2018-08-22 00:29:22 -0700

    Remove .only from tests

--- test/OracleBase.test.js
+++ test/OracleBase.test.js
@@ -6,7 +6,7 @@ require('chai').should()
 
 contract('OracleBase', (accounts) => {
 
-  it.only('is initialized with the correct state', async () => {
+  it('is initialized with the correct state', async () => {
     // const oracle = await OracleBase.new()
     // await expectRevert(oracle.resultFor(0))
     // const isResultSet = await oracle.isResultSet()
