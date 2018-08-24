978db7f905cb4390eb92e750adfae7bf80b48bf2
Chris Whinfrey <cwhinfrey@gmail.com>
2018-08-23 23:57:02 -0700

    Add BasicOracle NatSpec comments

--- contracts/BasicOracle.sol
+++ contracts/BasicOracle.sol
@@ -1,16 +1,28 @@
 pragma solidity ^0.4.24;
 
 import "./OracleBase.sol";
-import "zeppelin-solidity/contracts/ownership/Ownable.sol";
 
-contract BasicOracle is OracleBase, Ownable {
+/**
+ * @title BasicOracle
+ * @dev Extends OracleBase to allow the result to be set by a single data source
+ */
+contract BasicOracle is OracleBase {
 
+  // The address that is able to set the result
   address public dataSource;
 
+  /**
+   *  @dev BasicOracle constructor
+   *  @param _dataSource The address that is able to set the result
+   */
   constructor(address _dataSource) public {
     dataSource = _dataSource;
   }
 
+  /**
+   *  @dev Sets the result of the oracle
+   *  @param _result The result being set
+   */
   function setResult(bytes32 _result) public {
     require(msg.sender == dataSource, "The caller is not the data source.");
     _setResult(_result);
