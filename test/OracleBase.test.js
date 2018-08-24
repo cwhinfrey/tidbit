fc41869c14b6839425f9a70926d12106d0d0e2b8
Chris Whinfrey <cwhinfrey@gmail.com>
2018-08-23 18:22:05 -0700

    Add OracleBase test back in

--- test/OracleBase.test.js
+++ test/OracleBase.test.js
@@ -7,10 +7,10 @@ require('chai').should()
 contract('OracleBase', (accounts) => {
 
   it('is initialized with the correct state', async () => {
-    // const oracle = await OracleBase.new()
-    // await expectRevert(oracle.resultFor(0))
-    // const isResultSet = await oracle.isResultSet()
-    // isResultSet.should.equal(false)
+    const oracle = await OracleBase.new()
+    await expectRevert(oracle.resultFor(0))
+    const isResultSet = await oracle.isResultSet()
+    isResultSet.should.equal(false)
   })
 
 })
