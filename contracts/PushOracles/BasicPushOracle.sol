08a312132dde163a639a789fc0ede5fb6af96d0f
Chris Whinfrey <cwhinfrey@gmail.com>
2018-08-24 16:28:09 -0700

    Add BasicPushOracle natspec comments

--- contracts/PushOracles/BasicPushOracle.sol
+++ contracts/PushOracles/BasicPushOracle.sol
@@ -3,8 +3,18 @@ pragma solidity ^0.4.24;
 import "./PushOracleBase.sol";
 import "../BasicOracle.sol";
 
+/**
+ * @title BasicPushOracle
+ * @dev Combines BasicOracle and PushOracleBase to create a push style BasicOracle
+ */ 
 contract BasicPushOracle is BasicOracle, PushOracleBase {
 
+  /**
+   * @dev BasicPushOracle constructor
+   * @param _dataSource The address that is able to set the result
+   * @param _handler A contract that implements IOracleHandler and is called when
+   * the result has been set.
+   */
   constructor (
     address _dataSource,
     IOracleHandler _handler
