ae7cbad67c3f19528554550095902c2bba74053b
Alan Lu <alanlu1023@gmail.com>
2018-09-05 12:20:43 -0500

    Fix test variable shadowing

--- test/PushOracles/PushOracleBase.test.js
+++ test/PushOracles/PushOracleBase.test.js
@@ -7,8 +7,8 @@ contract('PushOracleBase', (accounts) => {
 
   it('is initialized with the correct state', async () => {
     const oracle = await PushOracleBase.new(handler)
-    const handler = await oracle.handler()
-    handler.should.equal(handler)
+    const handlerOnChain = await oracle.handler()
+    handler.should.equal(handlerOnChain)
   })
 
 })
