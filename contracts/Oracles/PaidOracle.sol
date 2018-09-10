43a9499ee7d0599d33a002a0a0f3a77f46c66fe2
Chris Whinfrey <cwhinfrey@gmail.com>
2018-09-10 10:41:09 -0700

    Move PaidOracle to oracles folder

--- contracts/Oracles/PaidOracle.sol
+++ contracts/Oracles/PaidOracle.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.4.24;
 
-import "./Oracles/BasicOracle.sol";
+import "./BasicOracle.sol";
 import "openzeppelin-solidity/contracts/math/Math.sol";
 import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 
