43500ed93530b65ffcb2c844a25b5ff5ee47903c
monoidz <caozheng1220@gmail.com>
2018-09-18 19:35:33 -0400

    test for multioracle; todo: will dataSource be set twice by a given id?

--- test/MultiOracle.test.js
+++ test/MultiOracle.test.js
@@ -1,3 +1,4 @@
+import { toAscii, fromAscii } from 'web3-utils'
 import expectRevert from './helpers/expectRevert'
 import expectEvent from './helpers/expectEvent'
 import { web3 } from './helpers/w3'
@@ -7,13 +8,58 @@ const MultiOracle = artifacts.require('MultiOracle')
 const BigNumber = web3.BigNumber;
 
 const should = require('chai')
-	.use(require('chai-bignumber')(BigNumber))
-	.should();
+  .use(require('chai-bignumber')(BigNumber))
+  .should();
 
 const RESULT = 'hello multiOracle'
 
 contract('MultiOracle', (accounts) => {
-	it('requires a non-null dataSource', async () => {
-		const oracle = await MultiOracle.new()
-	})
+  const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
+  const dataSource1 = accounts[1]
+  const dataSource2 = accounts[2]
+
+  let oracle
+  beforeEach(async ()=> {
+    oracle = await MultiOracle.new()
+  })
+
+  it('requires a non-null dataSource', async () => {
+    await expectRevert(oracle.newOracle(0, ZERO_ADDRESS))
+  })
+
+  // it('cannot set the same id twice', async () => {
+  //   await oracle.newOracle(0, dataSource1)
+  //   await expectRevert(oracle.newOracle(0, dataSource2))
+  // })
+
+  it('is initialized with the correct state', async () => {
+    await expectRevert(oracle.resultFor(1))
+    const isResultSet = await oracle.isResultSet(1)
+    isResultSet.should.equal(false)
+  })
+
+  it('can set result only by added dataSource', async () => {
+    await oracle.newOracle(1, dataSource1)
+    await expectRevert(oracle.setResult(1, RESULT, {from : dataSource2}))
+    const isResultSet = await oracle.isResultSet(1)
+    isResultSet.should.equal(false)
+
+    await oracle.setResult(1, RESULT, {from : dataSource1})
+    const result = await oracle.resultFor(1)
+    toAscii(result).replace(/\u0000/g, '').should.equal(RESULT)
+
+    const isResultSetSuccess = await oracle.isResultSet(1)
+    isResultSetSuccess.should.equal(true)
+  })
+
+  it('should emit ResultSet event', async () => {
+    const bytes32Result = fromAscii(RESULT)
+    await oracle.newOracle(2, dataSource2)
+    await expectEvent.inTransaction(
+      oracle.setResult(2, RESULT, { from: dataSource2 }),
+      'ResultSet',
+      { _result: bytes32Result, _sender: dataSource2 }
+    )
+  })
+  
 })
