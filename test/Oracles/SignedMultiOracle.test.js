c7c1ba2d514ef17c6b715afc3f92c156a3f80af3
Chris Whinfrey <cwhinfrey@gmail.com>
2019-02-21 14:04:38 -0800

    Set result with signature from third party account in SignedMultiOracle

--- test/Oracles/SignedMultiOracle.test.js
+++ test/Oracles/SignedMultiOracle.test.js
@@ -14,6 +14,7 @@ contract('SignedMultiOracle', (accounts) => {
   const signer0 = accounts[0]
   const signer1 = accounts[1]
   const signer2 = accounts[2]
+  const thirdParty = accounts[2]
   const id1 = soliditySha3(1)
   const id2 = soliditySha3(2)
 
@@ -29,14 +30,14 @@ contract('SignedMultiOracle', (accounts) => {
 
   it('can set result with signature by data source', async () => {
     const signature1 = await sign(messageHash1, signer1)
-    await oracle.setResultWithSignature(id1, RESULT1, signature1, { from: signer1 })
+    await oracle.setResultWithSignature(id1, RESULT1, signature1, { from: thirdParty })
     const result1 = await oracle.resultFor(id1)
     result1.should.equal(RESULT1)
     const isResultSet = await oracle.isResultSet(id1)
     isResultSet.should.equal(true)
 
     const signature2 = await web3.eth.sign(messageHash2, signer2)
-    await oracle.setResultWithSignature(id2, RESULT2, signature2, { from: signer2 })
+    await oracle.setResultWithSignature(id2, RESULT2, signature2, { from: thirdParty })
     const result2 = await oracle.resultFor(id2)
     result2.should.equal(RESULT2)
     const isResultSet2 = await oracle.isResultSet(id2)
@@ -45,27 +46,27 @@ contract('SignedMultiOracle', (accounts) => {
 
   it('cannot be set by a different signer', async () => {
     const signature = await web3.eth.sign(RESULT1, signer0)
-    await shouldFail(oracle.setResultWithSignature(id1, RESULT1, signature, { from: signer1}))
+    await shouldFail(oracle.setResultWithSignature(id1, RESULT1, signature, { from: thirdParty}))
   })
 
   it('cannot be set with the same id twice', async () => {
     let signature = await web3.eth.sign(messageHash1, signer1)
     await oracle.setResultWithSignature(id1, RESULT1, signature, { from: signer1 })
-    await shouldFail(oracle.setResultWithSignature(id1, RESULT1, signature, { from: signer1 }))
+    await shouldFail(oracle.setResultWithSignature(id1, RESULT1, signature, { from: thirdParty }))
 
     const secondHash = web3.utils.soliditySha3('another result');
     signature = await web3.eth.sign(secondHash, signer1)
-    await shouldFail(oracle.setResultWithSignature(id1, RESULT1, signature, { from: signer1 }))
+    await shouldFail(oracle.setResultWithSignature(id1, RESULT1, signature, { from: thirdParty }))
   })
 
   it('should emit ResultSet event', async () => {
     // hard-coded here since web3.utils.fromAscii won't return 32 bytes hex string
     const signature = await web3.eth.sign(messageHash1, signer1)
-    const { logs } = await oracle.setResultWithSignature(id1, RESULT1, signature, { from: signer1 })
+    const { logs } = await oracle.setResultWithSignature(id1, RESULT1, signature, { from: thirdParty })
     await expectEvent.inLogs(
       logs,
       'ResultSet',
-      {_id: id1, _result: RESULT1, _sender: signer1 }
+      {_id: id1, _result: RESULT1, _sender: thirdParty }
     )
   })
 })
